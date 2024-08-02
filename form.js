7688004bed5c1f8065a25be1fe97f9bafbaadddf
omastrangelo <octamastrangelo@gmail.com>
2024-08-02 19:32:41 -0300

    Correcciones de archivos

--- form.js
+++ form.js
@@ -12,11 +12,11 @@ btnEnviar.addEventListener("click", ()=>{
         return; // esto lo vi en la clase de Mauricio, leer documentacion para entender su funcionamiento.
     }
 
-    informacion[0] = nombre.value;
-    informacion[1] = apellido.value;
-    informacion[2] = tel.value;
-    informacion[3] = email.value;
-    informacion[4] = texto.value;
+    informacion[0] = "Nombre: " + nombre.value;//pone el valor que se escribio en el input nombre en el primer elemento
+    informacion[1] = "Apellido: " +  apellido.value;//pone el valor que se escribio en el input apellido en el segundo elemento
+    informacion[2] = "Teléfono: " + tel.value; //pone el valor que se escribio en el input telefono en el tercero elemento
+    informacion[3] = "Email: " + email.value; //pone el valor que se escribio en el input email en el cuarto elemento
+    informacion[4] = "Mensaje: " + texto.value; //pone el valor que se escribio en el input mensaje en el quinto elemento
     let blob = new Blob([informacion], {type:"text/plain;charset=utf-8"});
     //blob es un objeto de tipo fichero de datos planos inmutables convierte lo que ingresamos en formato texto plano.
     //uso de libreria filesaver
